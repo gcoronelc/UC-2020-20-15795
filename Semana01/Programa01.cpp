e8050b456e5883c6cbe66a31736f34d2b72d9f55
Gustavo Coronel <gcoronelc@gmail.com>
2021-08-16 08:51:35 -0500

    Semana 1

---
+++ Semana01/Programa01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	cout << "Hola todos, bienvenidos al curso." << endl;
+	cout << "Espero que se diviertan mucho." << endl;
+	cout << endl;
+	
+	return 0;
+	
+}
