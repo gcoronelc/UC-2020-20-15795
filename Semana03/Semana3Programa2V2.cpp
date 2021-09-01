cc1a2be38a193f1ba8686a10e6448832086d59f4
Gustavo Coronel <gcoronelc@gmail.com>
2021-09-01 08:58:59 -0500

    Semana 3

---
+++ Semana03/Semana3Programa2V2.cpp
@@ -0,0 +1,43 @@
+/*
+ * Programa que calcula la suma o el producto de 2 números.
+ */
+#include<iostream>
+#include <sstream>
+using namespace std;
+
+int main( )
+{ 	
+	// Configuración
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Variables
+	int a, b;
+	int c; 
+	stringstream reporte; // Es un buffer
+	
+	// Lectura de datos
+	cout<<"Ingrese valor de primer número:\t\t"; cin>>a;
+	cout<<"Ingrese valor de segundo número:\t";  cin>>b;
+
+	// Proceso
+	if( a>0 && b>0 )
+	{
+		c = a + b;
+		reporte << "La suma es:\t\t\t\t" << c << "\n";
+	}
+	else
+	{
+		c = a * b;
+		reporte << "La multiplicación es:\t\t\t" << c << "\n";
+	}
+	
+	// Reporte
+	cout << endl;
+	cout << reporte.str() << endl;
+	
+	// Fin
+	return 0;
+}
+
+
+
