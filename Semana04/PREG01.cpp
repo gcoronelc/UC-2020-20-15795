216f0288cd51eda1c68eb8ae484b56b74ea14431
Gustavo Coronel <gcoronelc@gmail.com>
2021-09-06 08:48:36 -0500

    Semana 04

---
+++ Semana04/PREG01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	int i = 10;
+	if( i=20 == 10 )
+		cout << i << endl;
+		cout << "Bien\n";
+	
+	// Fin
+	system("pause");
+	return 0;
+}
