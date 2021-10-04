2d7dce1b4b1f2a42bdc93ff802a94052e59da64d
Gustavo Coronel <gcoronelc@gmail.com>
2021-10-04 08:58:55 -0500

    Semana 08

--- Semana08/SIMULACRO_EP.cpp
+++ Semana08/SIMULACRO_EP.cpp
@@ -10,6 +10,9 @@ int main(){
 	int opMenu;
 	int tipoPlato, cantidad, edad;
 	char sexo;
+	double porcDcto1, porcDcto2, precio;
+	string unidad;
+	double importeVenta, descuento1, descuento2, importeNeto;
 	
 	// Control del menú de opciones
 	do{
@@ -62,6 +65,12 @@ int main(){
 					}
 				}while(cantidad <= 0);
 				do{
+					cout << "Precio: "; cin >> precio;
+					if(precio <= 0){
+						cout << "ERROR, El precio es un valor positivo, intentar nuevamente." << endl;
+					}
+				}while(precio <= 0);
+				do{
 					cout << "Sexo (M/F): "; cin >> sexo;
 					sexo = toupper(sexo);
 					if(sexo!= 'M' && sexo!='F'){
@@ -74,7 +83,43 @@ int main(){
 						cout << "ERROR, la edad es un valor positivo, intentar nuevamente." << endl;
 					}
 				}while(edad <= 0);
+				// Porcentaje de descuento 1
+				switch(tipoPlato){
+					case 1:
+						unidad = "Plato(s)";
+						porcDcto1 = 0.0; // Valor inicial
+						porcDcto1 = (cantidad>=5 && cantidad<=10)?0.05:porcDcto1;
+						porcDcto1 = (cantidad>10)?0.08:porcDcto1;
+					case 2:
+						unidad = "Plato(s)";
+						porcDcto1 = 0.0; // Valor inicial
+						porcDcto1 = (cantidad>=5 && cantidad<=10)?0.06:porcDcto1;
+						porcDcto1 = (cantidad>10)?0.10:porcDcto1;
+					case 3:
+						unidad = "Vino(s))";
+						porcDcto1 = 0.0; // Valor inicial
+						porcDcto1 = (cantidad>=5 && cantidad<=10)?0.08:porcDcto1;
+						porcDcto1 = (cantidad>10)?0.14:porcDcto1;							
+				}
 				
+				// Porcentaje de descuento 2
+				porcDcto2 = 0.0; // Valor inicial
+				porcDcto2 = (sexo=='M' && edad >=30 && edad<=40)?0.06:porcDcto2;
+				porcDcto2 = (sexo=='M' && edad >40)?0.08:porcDcto2;
+				porcDcto2 = (sexo=='F' && edad >=30 && edad<=40)?0.08:porcDcto2;
+				porcDcto2 = (sexo=='F' && edad >40)?0.10:porcDcto2;
+				// Calculos
+				importeVenta = precio * cantidad;
+				descuento1 = importeVenta * porcDcto1;
+				descuento2 = importeVenta*porcDcto2;
+				importeNeto = importeVenta - descuento1 - descuento2;
+				cout << endl;
+				cout << "REPORTE" << endl;
+				cout << "=================================================" << endl;				
+				cout << "Importe de venta: " << importeVenta << endl;
+				cout << "Descuento 1: " << descuento1 << endl;
+				cout << "Descuento 2: " << descuento2 << endl;
+				cout << "Importe neto: " << importeNeto << endl;
 				
 				break;
 				
@@ -84,16 +129,13 @@ int main(){
 			case 2:
 				cout << "Proceso opción 2" << endl;
 				break;	
+				
 			// Opción 3: cccccccc
 			case 3:
 				cout << "Proceso opción 3" << endl;
 				break;					
 			// Opción 4: dddddddd
 			case 4:
-				cout << "Proceso opción 4" << endl;
-				break;
-			// Opción 5: Salir
-			case 5:
 				cout << "Fin del programa." << endl;
 				break;
 		}
