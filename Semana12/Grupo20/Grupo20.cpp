9bf66a7b6329d9760403b97bfb59d665b6b85589
Gustavo Coronel <gcoronelc@gmail.com>
2021-11-03 07:59:44 -0500

    Semana 12

---
+++ Semana12/Grupo20/Grupo20.cpp
@@ -0,0 +1,32 @@
+/*
+GRUPO 20: Los Mejores
+- Gustavo Coronel
+- JEAN FRANCO SAAVEDRA CAMIZAN
+- NEISER VASQUEZ LOZANO
+*/
+#include <iostream>
+#include "LecturaDatos.h"
+#include "LibreriaEGCC.h"
+using namespace std;
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Variables
+	
+	
+	// Lectura de datos
+	
+	
+	// Proceso
+	
+	
+	// Reporte
+	
+	
+	// Fin
+	system("pause");
+	return 0;
+}
