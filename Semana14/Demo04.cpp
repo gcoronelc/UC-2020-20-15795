1260c538cab5dc5a7c05c4af89b31a9ac20972e1
Gustavo Coronel <gcoronelc@gmail.com>
2021-11-15 08:57:29 -0500

    Semana 14

--- Semana14/Demo04.cpp
+++ Semana14/Demo04.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
 
+long fnMultiplicar(int a, int b){
+	if( a==0 || b==0 ){
+		return 0;
+	}
+	if( b==1 ){
+		return a;
+	}
+	return a + fnMultiplicar(a,b-1);
+}
+
 long fnPotencia(int x, int y){
 	if( y==0 ){
 		return 1;
@@ -9,18 +19,19 @@ long fnPotencia(int x, int y){
 }
 
 int main(){
-	
 	// Configuracion
 	setlocale(LC_CTYPE,"Spanish");
 	// Variables
-	int x, y, potencia;
+	int x, y, potencia, producto;
 	// Datos
 	x = 3;
 	y = 4;
 	// Proceso
 	potencia = fnPotencia(x,y);
+	producto = fnMultiplicar(x,y);
 	// Reporte
 	cout << "Potencia = " << potencia << endl;
+	cout << "Producto = " << producto << endl;
 	// Fin
 	system("pause");
 	return 0;
